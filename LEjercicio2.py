8283676d19004916fa56130e16aef97f382e7eb9
juanverar <40802443+juanverar@users.noreply.github.com>
2018-11-28 19:35:24 -0500

    Ejercicio 2 terminado.

--- LEjercicio2.py
+++ LEjercicio2.py
@@ -9,6 +9,16 @@ print("-------------------------------------------------------------------------
 
 import numpy as np
 import matplotlib.pylab as plt
+
 u = np.array([12.,45.,6.,78.,34.,22.,-10.,31.,-27.])
 v = np.array([3.,11.,1.3,37.,11.,6.,-23.,7.,7.])
+
+mu=np.mean(u)
+mv=np.mean(v)
+
+def cov(u,v):
+	for i in range(len(u)):	
+		return (1/(len(u)-1.0))*np.sum((u[i]-mu)*(v[i]-mv))
+
+print 'La covarianza entre u y v tiene un valor de= ',cov(u,v)
 
